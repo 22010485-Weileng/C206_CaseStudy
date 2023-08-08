888bd5f2aff111e1e1e8da31eb60105594758d0c
wei leng <wei leng@adelyn>
2023-08-08 13:51:53 +0800

    c206

--- C206_CaseStudy/test/C206_CaseStudyTest.java
+++ C206_CaseStudy/test/C206_CaseStudyTest.java
@@ -1,18 +1,32 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class C206_CaseStudyTest {
-	private currency;
+	private Currency cur1;
+	private Currency cur2;
+	private Currency cur3;
+	ArrayList<Currency> currencyList = new ArrayList<Currency>();
 
 	@Before
 	public void setUp() throws Exception {
+		cur1 = new Currency("MYR","Malaysian Ringgit", 3.40);
+		cur2 = new Currency("CNY", "Yuan Renminbi", 5.35);
+		cur3 = new Currency("USD", "US Dollar", 0.75);
+		
+		currencyList= new ArrayList<Currency>();
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		cur1 = null;
+		cur2 = null;
+		cur3 = null;
+		currencyList = null;
 	}
 
 	@Test
